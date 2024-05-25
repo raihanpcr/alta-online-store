f027fe9bbd1757ffb619adc312172c90fb28cddf
raihanpcr <raihanalrasyid15@gmail.com>
2024-05-25 21:15:42 +0700

    pertemuan 1 finished

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,12 @@
+//route dianggap sebagai controller
 import { NextResponse } from "next/server";
 
 export async function GET() {
+      //return response berupa json
       return NextResponse.json({message : "Success GET",data:[]});
+}
+
+//API with POST method
+export async function POST() {
+      return NextResponse.json({message: "Success POST",data:[]});
 }
---
+++ src/app/product/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+      return <div>Page Product
+            <Button variant="outline">Click Me!</Button>
+      </div>
+}
